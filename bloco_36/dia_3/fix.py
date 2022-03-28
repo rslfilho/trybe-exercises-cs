10d2f98d3b6c65862c23e93cfb01baef9edc817e
Roberval Filho <rslfilho@gmail.com>
2022-03-28 14:52:40 -0300

    Exercícios de fixação do dia 36.3

---
+++ bloco_36/dia_3/fix.py
@@ -0,0 +1,51 @@
+class Employee:
+    def __init__(self, id_num, name):
+        self.id_num = id_num
+        self.name = name
+
+
+class HashMap:
+    def __init__(self):
+        self._buckets = [[] for i in range(10)]
+
+    def get_address(self, id_num):
+        return id_num % 10
+
+    def insert(self, employee):
+        address = self.get_address(employee.id_num)
+        self._buckets[address].append(employee)
+
+    def get_value(self, id_num):
+        address = self.get_address(id_num)
+        for item in self._buckets[address]:
+            if item.id_num == id_num:
+                return item.name
+        return None
+
+    def has(self, id_num):
+        address = self.get_address(id_num)
+        for item in self._buckets[address]:
+            if item.id_num == id_num:
+                return True
+        return False
+
+    def update_value(self, id_num, new_name):
+        address = self.get_address(id_num)
+        for item in self._buckets[address]:
+            if item.id_num == id_num:
+                item.name = new_name
+
+
+employees = [(14, "name1"), (23, "name2"), (10, "name3"), (9, "name4")]
+
+hashmap = HashMap()
+
+for employee in employees:
+    employee_instance = Employee(employee[0], employee[1])
+    hashmap.insert(employee_instance)
+
+print(hashmap.get_value(23))
+
+hashmap.update_value(10, 'name30')
+
+print(hashmap.get_value(10))
